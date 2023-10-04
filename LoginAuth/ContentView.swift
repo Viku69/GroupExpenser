cf76c6f41f19208c3ffa9d92a97bfa6a36e0da97
Vikram Singh <113547823+Viku69@users.noreply.github.com>
2023-10-04 19:55:14 +0530

    Initial Commit

---
+++ LoginAuth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginAuth
+//
+//  Created by Vikram Singh on 04/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginAuth/LoginAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAuthApp.swift
+//  LoginAuth
+//
+//  Created by Vikram Singh on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
